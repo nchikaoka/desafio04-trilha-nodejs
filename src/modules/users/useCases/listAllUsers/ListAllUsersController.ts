745dcec2709a087ba5b6a2dd1f6693162f87dc1f
nchikaoka <natan.chikaoka@gmail.com>
2021-11-20 16:11:23 -0300

    implemented error handling in the ListAllUsers and ShowUserProfile routes. also correted the retrieval of id from request.headers and the json return in the ShowUserProfileController.

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,10 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params;
-    const userList = this.listAllUsersUseCase.execute({ user_id });
+    const { user_id } = request.headers;
+    try {
+      const userList = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
 
-    return response.json(userList);
+      return response.json(userList);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
